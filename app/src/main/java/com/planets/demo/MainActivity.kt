a6400906c35ceb6c59dfed4edef470f3b3da24e4
aman <reachttoaman@gmail.com>
2024-04-13 15:38:50 +0100

    Initial commit

---
+++ app/src/main/java/com/planets/demo/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.planets.demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Greeting("World")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
